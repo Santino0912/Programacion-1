83203ecfde460f8257d08361b4ee8e0292ae1754
santino0912 <ssantinomunnoz@gmail.com>
2022-04-08 11:09:28 -0300

    ejercicio3 funciones con retorno, template strings

---
+++ funciones_retorno/index.js
@@ -0,0 +1,21 @@
+function calcular(){
+
+    const num1 = 2
+
+    const num2 = 3
+
+    const resultado = Math.pow(num2,num1)
+
+    return resultado
+
+}
+
+
+
+function mostrar_resultado(){
+    //invocamos la funcion y recibimos una respuesta
+    const respuesta = calcular()
+
+    alert(respuesta)
+
+}
---
+++ template_string/index.js
@@ -0,0 +1,21 @@
+function mostrar(){
+    
+    const nombre_apellido = 'Santino Muñoz Florit'
+    console.log('Nombre y apellido: '+nombre_apellido)
+
+
+}
+
+
+
+function mostrar2(){
+    const nombre_apellido = 'Santino Muñoz Florit'
+    
+    
+    //template string _ backkicks de javascript
+    console.log(`
+        Nombre y Apellido: ${nombre_apellido}
+        Dni: 123456
+        
+    `)
+}
